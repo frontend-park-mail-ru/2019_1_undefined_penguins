d1b8c147f2639a27f944ac6c387ca8968f2474d1
Betchika99 <dobrl2011@mail.ru>
2019-03-10 00:48:21 +0300

    FRONT-13: Added eslint linter

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 2019_1_undefined_penguins
+
+# ESLint
+
+ESLint позволяет проводить анализ качества вашего кода, написанного на любом выбранном стандарте JavaScript.
+
+Установка пакета ```npm install --save-dev eslint```
+
+Запуск ```./node_modules/.bin/eslint <filename>.js```
+
+Запуск с исправлением некоторых багов ```./node_modules/.bin/eslint <filename>.js --fix```
--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const express = require('express');
 const body = require('body-parser');
@@ -6,6 +6,7 @@ const cookie = require('cookie-parser');
 const morgan = require('morgan');
 const uuid = require('uuid/v4');
 const path = require('path');
+
 const app = express();
 
 
@@ -14,79 +15,67 @@ app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(body.json());
 app.use(cookie());
 
-// сохраняет файлы по пути './static/avatars' 
+// сохраняет файлы по пути './static/avatars'
 // с имененем file.fieldname + '-' + Date.now() + тип файла
-const storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null, './public/uploads')
-    },
-    filename: function (req, file, callback) {
-        callback(null, file.fieldname + '-' + Date.now() + '.' + ((file.mimetype === 'image/png') ? 'png' : 'jpeg'))
-    }
-});
-
-const upload = multer({storage: storage});
-
-
 
 /* sets menu.html as root */
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
- });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
 
 const users = {
-	'a.penguin1@corp.mail.ru': {
-		login: 'Penguin1',
-		email: 'a.penguin1@corp.mail.ru',
-		password: 'password',
-		name: 'Пингвин Северного Полюса',
-		lastVisit: '25.02.2019',
-		score: 0,
-		avatarName: 'default1.png',
-		avatarBlob: ''
-	},
-	'b.penguin2@corp.mail.ru': {
-		login: 'Penguin2',
-		email: 'b.penguin2@corp.mail.ru',
-		password: 'password',
-		name: 'Пингвин Южного Полюса',
-		lastVisit: '26.02.2019',
-		score: 100500,
-		avatarName: 'default2.png',
-		avatarBlob: ''
-	},
-	'c.penguin3@corp.mail.ru': {
-		login: 'Penguin3',
-		email: 'c.pengin3@corp.mail.ru',
-		password: 'password',
-		name: 'Залетный Пингвин',
-		lastVisit: '14.02.2019',
-		score: 172,
-		avatarName: 'default3.png',
-		avatarBlob: ''
-	},
-	'd.penguin4@corp.mail.ru': {
-		login: 'Penguin4',
-		email: 'd.penguin4@corp.mail.ru',
-		password: 'password',
-		name: 'Рядовой Пингвин',
-		lastVisit: '15.02.2019',
-		score: 72,
-		avatarName: 'default4.png',
-		avatarBlob: ''
-	},
+  'a.penguin1@corp.mail.ru': {
+    login: 'Penguin1',
+    email: 'a.penguin1@corp.mail.ru',
+    password: 'password',
+    name: 'Пингвин Северного Полюса',
+    lastVisit: '25.02.2019',
+    score: 0,
+    avatarName: 'default1.png',
+    avatarBlob: '',
+  },
+  'b.penguin2@corp.mail.ru': {
+    login: 'Penguin2',
+    email: 'b.penguin2@corp.mail.ru',
+    password: 'password',
+    name: 'Пингвин Южного Полюса',
+    lastVisit: '26.02.2019',
+    score: 100500,
+    avatarName: 'default2.png',
+    avatarBlob: '',
+  },
+  'c.penguin3@corp.mail.ru': {
+    login: 'Penguin3',
+    email: 'c.pengin3@corp.mail.ru',
+    password: 'password',
+    name: 'Залетный Пингвин',
+    lastVisit: '14.02.2019',
+    score: 172,
+    avatarName: 'default3.png',
+    avatarBlob: '',
+  },
+  'd.penguin4@corp.mail.ru': {
+    login: 'Penguin4',
+    email: 'd.penguin4@corp.mail.ru',
+    password: 'password',
+    name: 'Рядовой Пингвин',
+    lastVisit: '15.02.2019',
+    score: 72,
+    avatarName: 'default4.png',
+    avatarBlob: '',
+  },
 };
 
 const ids = {};
 
-app.use( (req, res, next) => {
-		res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-		res.setHeader('Access-Control-Allow-Credentials', 'true');
-		res.setHeader('Access-Control-Allow-Headers', 'content-type');
-		res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
 
-	next();
+
+  next();
 });
 
 // app.use(function (req, res, next) {
@@ -97,109 +86,105 @@ app.use( (req, res, next) => {
 //     next();
 // });
 
-app.post('/signup', function (req, res) {
-	const password = req.body.password;
-	const email = req.body.email;
-	if (
-		!password || !email ||
-		!password.match(/^\S{4,}$/) ||
-		!email.match(/@/)
-	) {
-		return res.status(400).json({error: 'Невалидные данные пользователя'});
-	}
-	if (users[email]) {
-		return res.status(400).json({error: 'Пользователь уже существует'});
-	}
+app.post('/signup', (req, res) => {
+  const { password } = req.body;
+  const { email } = req.body;
+  if (
+    !password || !email
+		|| !password.match(/^\S{4,}$/)
+		|| !email.match(/@/)
+  ) {
+    return res.status(400).json({ error: 'Невалидные данные пользователя' });
+  }
+  if (users[email]) {
+    return res.status(400).json({ error: 'Пользователь уже существует' });
+  }
 
-	const id = uuid();
-	const user = {password, email, score: 0};
-	ids[id] = email;
-	users[email] = user;
+  const id = uuid();
+  const user = { password, email, score: 0 };
+  ids[id] = email;
+  users[email] = user;
 
-	res.cookie('sessionid', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
-	res.status(201).json({id});
+  res.cookie('sessionid', id, { expires: new Date(Date.now() + 1000 * 60 * 10) });
+  res.status(201).json({ id });
 });
 
-app.post('/login', function (req, res) {
-	const password = req.body.password;
-	const email = req.body.email;
-	if (!password || !email) {
-		return res.status(400).json({error: 'Не указан E-Mail или пароль'});
-	}
-	if (!users[email] || users[email].password !== password) {
-		return res.status(400).json({error: 'Неверный E-Mail и/или пароль'});
-	}
+app.post('/login', (req, res) => {
+  const { password } = req.body;
+  const { email } = req.body;
+  if (!password || !email) {
+    return res.status(400).json({ error: 'Не указан E-Mail или пароль' });
+  }
+  if (!users[email] || users[email].password !== password) {
+    return res.status(400).json({ error: 'Неверный E-Mail и/или пароль' });
+  }
 
-	const id = uuid();
-	ids[id] = email;
+  const id = uuid();
+  ids[id] = email;
 
-	res.cookie('sessionid', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
-	res.status(200).json({id});
+  res.cookie('sessionid', id, { expires: new Date(Date.now() + 1000 * 60 * 10) });
+  res.status(200).json({ id });
 });
 
-app.get('/signout', function (req, res){
-	const id = req.cookies['sessionid'];
-	const email = ids[id];
-	if (!email || !users[email]) {
-		return res.status(401).end();
-	}
-	res.clearCookie('sessionid');
-	res.json({ status: 'successfully signed out' })
-
+app.get('/signout', (req, res) => {
+  const id = req.cookies.sessionid;
+  const email = ids[id];
+  if (!email || !users[email]) {
+    return res.status(401).end();
+  }
+  res.clearCookie('sessionid');
+  res.json({ status: 'successfully signed out' });
 });
 
-app.get('/me', function (req, res) {
-	const id = req.cookies['sessionid'];
-	const email = ids[id];
-	if (!email || !users[email]) {
-		return res.status(401).end();
-	}
+app.get('/me', (req, res) => {
+  const id = req.cookies.sessionid;
+  const email = ids[id];
+  if (!email || !users[email]) {
+    return res.status(401).end();
+  }
 
-	users[email].score += 1;
+  users[email].score += 1;
 
-	res.json(users[email]);
+  res.json(users[email]);
 });
 
-app.post('/change_profile', function (req, res) {
-	const id = req.cookies['sessionid'];
-	const email = ids[id];
-	if (!email || !users[email]) {
-		return res.status(401).end();
-	}
+app.post('/change_profile', (req, res) => {
+  const id = req.cookies.sessionid;
+  const email = ids[id];
+  if (!email || !users[email]) {
+    return res.status(401).end();
+  }
 
-	ids[id] = email;
+  ids[id] = email;
 
-	users[email].email = req.body.email;
-	users[email].login = req.body.login;
-	users[email].name = req.body.name;
-	users[email].avatarName = req.body.avatarName;
-	users[email].avatarBlob = req.body.avatarBlob;
+  users[email].email = req.body.email;
+  users[email].login = req.body.login;
+  users[email].name = req.body.name;
+  users[email].avatarName = req.body.avatarName;
+  users[email].avatarBlob = req.body.avatarBlob;
 
-	//what for?
-	res.cookie('sessionid', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
-	res.status(201).json({id});
+  // what for?
+  res.cookie('sessionid', id, { expires: new Date(Date.now() + 1000 * 60 * 10) });
+  res.status(201).json({ id });
 });
 
 // app.get('/about', function (req, res) {
 
 // });
 
-app.get('/leaders', function (req, res) {
-	const scorelist = Object.values(users)
-		.sort((l, r) => r.score - l.score)
-		.map(user => {
-			return {
-				email: user.email,
-				score: user.score,
-			}
-		});
-	res.json(scorelist);
-	// res.status(200).json(scorelist);;
+app.get('/leaders', (req, res) => {
+  const scorelist = Object.values(users)
+    .sort((l, r) => r.score - l.score)
+    .map(user => ({
+      email: user.email,
+      score: user.score,
+    }));
+  res.json(scorelist);
+  // res.status(200).json(scorelist);;
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, function () {
-	console.log(`Server listening port ${port}`);
+app.listen(port, () => {
+  console.log(`Server listening port ${port}`);
 });
-
