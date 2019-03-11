7f4146d417b89b0aef8c93aa24ae084c8ec4e391
Betchika99 <dobrl2011@mail.ru>
2019-03-11 16:10:00 +0300

    develop: fixed bugs in cors

--- server/index.js
+++ server/index.js
@@ -68,7 +68,7 @@ app.use( (req, res, next) => {
 		res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
 		res.setHeader('Access-Control-Allow-Credentials', 'true');
 		res.setHeader('Access-Control-Allow-Headers', 'content-type');
-		res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+		res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE, OPTIONS");
     
 	next();
 });
