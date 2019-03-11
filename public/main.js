d70159ce0f00356406f1aa0d73c28f90c81ba66a
Vitaly <79vitaly79@gmail.com>
2019-03-11 19:13:29 +0300

    Fixed bugs from first review

--- public/main.js
+++ public/main.js
@@ -16,9 +16,9 @@ const application = document.getElementById('application');
  * Создать ссылку на главное меню
  * @return  ссылка на главное меню
  */
-function createMenuLink () {
-	const menuLink = document.createElement('a');
-	menuLink.href = menuLink.dataset.href = 'menu';
+function createMenuLink() {
+  const menuLink = document.createElement('a');
+  menuLink.href = menuLink.dataset.href = 'menu';
   menuLink.textContent = 'Back to main menu';
 
   return menuLink;
@@ -27,142 +27,142 @@ function createMenuLink () {
 /**
  * Создание главного меню
  */
-function createMenu (errors) {
-	const menuSection = document.createElement('section');
-	menuSection.dataset.sectionName = 'menu';
+function createMenu(errors) {
+  const menuSection = document.createElement('section');
+  menuSection.dataset.sectionName = 'menu';
 
-	const menu = new MenuComponent({
-		el: menuSection,
-		type: RENDER_TYPES.DOM,
-	});
-	menu.header = 'Penguin\'s Wars';
-	menu.render();
-	if (errors) {
-		const errorsSection	= document.createElement('section');
-		errorsSection.dataset.sectionName = 'errors';
-		const err = document.createElement('span');
-		err.classList.add('errorLabel');
-		err.innerText = errors;
-		errorsSection.appendChild(err);
-		application.appendChild(errorsSection);
-	}
+  const menu = new MenuComponent({
+    el: menuSection,
+    type: RENDER_TYPES.DOM,
+  });
+  menu.header = 'Penguin\'s Wars';
+  menu.render();
+  if (errors) {
+    const errorsSection	= document.createElement('section');
+    errorsSection.dataset.sectionName = 'errors';
+    const err = document.createElement('span');
+    err.classList.add('errorLabel');
+    err.innerText = errors;
+    errorsSection.appendChild(err);
+    application.appendChild(errorsSection);
+  }
 
   application.appendChild(menuSection);
 }
 /**
  * Создание страницы авторизации
  */
-function createSignIn () {
-	const signInSection = document.createElement('section');
-	signInSection.dataset.sectionName = "sign_in"
+function createSignIn() {
+  const signInSection = document.createElement('section');
+  signInSection.dataset.sectionName = 'sign_in';
 
-	const signIn = new SignInComponent({
-		el: signInSection,
-		type: RENDER_TYPES.TMPL,
-	})
+  const signIn = new SignInComponent({
+    el: signInSection,
+    type: RENDER_TYPES.TMPL,
+  });
 
-	signIn.render();  
-	application.appendChild(signInSection);
+  signIn.render();
+  application.appendChild(signInSection);
 
-	const form = document.getElementsByTagName('form')[0];
-	form.addEventListener('submit', function (event) {
-		event.preventDefault();
+  const form = document.getElementsByTagName('form')[0];
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-		let email = form.elements[ 'email' ].value;
-		let password = form.elements[ 'password' ].value;
-            
-		if (signIn.status !== 200) {
-			email = '';
-			password = '';	
-			signIn.status = 200;	
-		}
-		else {
-		AjaxModule.doPromisePost({
-			path: '/login',
-			body: {
-				email: email,
-				password: password,
-			},
-		})
-		.then (
-			(data) => {
-				console.log(JSON.stringify(data));
-				if(data.status > 300) {
-					throw new Error('Network response was not ok.'); 
-				}
-				return data;  
-			})
-		.then( () => {
-			application.innerHTML = '';
-			createProfile();
-		})
-		.catch( () => {
-			console.error;
-			application.innerHTML = '';
-			createMenu();
-		});
-	}
-	});
-	application.appendChild(createMenuLink());
+    let email = form.elements.email.value;
+    let password = form.elements.password.value;
+
+    if (signIn.status !== 200) {
+      email = '';
+      password = '';
+      signIn.status = 200;
+    } else {
+      AjaxModule.doPromisePost({
+        path: '/login',
+        body: {
+          email,
+          password,
+        },
+      })
+        .then(
+          (data) => {
+            console.log(JSON.stringify(data));
+            if (data.status > 300) {
+              throw new Error('Network response was not ok.');
+            }
+            return data;
+          },
+        )
+        .then(() => {
+          application.innerHTML = '';
+          createProfile();
+        })
+        .catch(() => {
+          console.error;
+          application.innerHTML = '';
+          createMenu();
+        });
+    }
+  });
+  application.appendChild(createMenuLink());
 }
 
 /**
  * Создание страницы регистрации
  */
-function createSignUp () {
-	const signUpSection = document.createElement('section');
-	signUpSection.dataset.sectionName = 'sign_up';
+function createSignUp() {
+  const signUpSection = document.createElement('section');
+  signUpSection.dataset.sectionName = 'sign_up';
 
   const signUp = new SignUpComponent({
-		el: signUpSection,
+    el: signUpSection,
     type: RENDER_TYPES.TMPL,
-	})
+  });
 
-	signUp.render();
+  signUp.render();
 
-	application.appendChild(signUpSection);	
+  application.appendChild(signUpSection);
 
-	const form = document.getElementsByTagName('form')[0];
+  const form = document.getElementsByTagName('form')[0];
 
-	form.addEventListener('submit', function (event) {
-		event.preventDefault();
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-	let email = form.elements[ 'email' ].value;
-	let password = form.elements[ 'password' ].value;
+    let email = form.elements.email.value;
+    let password = form.elements.password.value;
 
-	if (signUp.status !== 200) {
-		email = '';
-		password = '';
-		signUp.status = 200;
-	}
-	else {
-		AjaxModule.doPromisePost({
-			path: '/signup',
-			body: {
-				email: email,
-				password: password
-			},	
-		})
-		.then (
-			(data) => {
-				console.log(JSON.stringify(data));
-				if(data.status > 300) {
-					throw new Error('Network response was not ok.'); 
-				}
-				return data; 
-			})
-		.then( () => {
-			application.innerHTML = '';
-			createProfile();
-		})
-		.catch( () => {
-			console.error;
-			application.innerHTML = '';
-			createMenu();
-		});          
-	}
-	})
-	application.appendChild(createMenuLink());
+    if (signUp.status !== 200) {
+      email = '';
+      password = '';
+      signUp.status = 200;
+    } else {
+      AjaxModule.doPromisePost({
+        path: '/signup',
+        body: {
+          email,
+          password,
+        },
+      })
+        .then(
+          (data) => {
+            console.log(JSON.stringify(data));
+            if (data.status > 300) {
+              throw new Error('Network response was not ok.');
+            }
+            return data;
+          },
+        )
+        .then(() => {
+          application.innerHTML = '';
+          createProfile();
+        })
+        .catch(() => {
+          console.error;
+          application.innerHTML = '';
+          createMenu();
+        });
+    }
+  });
+  application.appendChild(createMenuLink());
 }
 
 /**
@@ -170,155 +170,151 @@ function createSignUp () {
  * @param users - Массив пользователей
  * @param pageNumber - Номер страницы
  */
-function createLeaderboard (users, pageNumber = 0) {
+function createLeaderboard(users, pageNumber = 0) {
+  const leaderboardSection = document.createElement('section');
+  leaderboardSection.dataset.sectionName = 'leaders';
 
-	const leaderboardSection = document.createElement('section');
-	leaderboardSection.dataset.sectionName = 'leaders';
+  const header = document.createElement('h1');
+  header.textContent = 'Leaders';
 
-	const header = document.createElement('h1');
-	header.textContent = 'Leaders';
+  const boardWrapper = document.createElement('div');
 
-	const boardWrapper = document.createElement('div');
+  leaderboardSection.appendChild(header);
+  leaderboardSection.appendChild(createMenuLink());
+  leaderboardSection.appendChild(document.createElement('br'));
+  leaderboardSection.appendChild(boardWrapper);
+  const itemsNumber = 3;
+  if (users) {
+    const board = new BoardComponent({
+      el: boardWrapper,
+      type: RENDER_TYPES.TMPL,
+    });
+    board.data = JSON.parse(JSON.stringify(users));
+    board.render();
+  } else {
+    const em = document.createElement('em');
+    em.textContent = 'Loading';
+    leaderboardSection.appendChild(em);
 
-	leaderboardSection.appendChild(header);
-	leaderboardSection.appendChild(createMenuLink());
-	leaderboardSection.appendChild(document.createElement('br'));
-	leaderboardSection.appendChild(boardWrapper);
-	const itemsNumber = 3
-	if (users) {
-		const board = new BoardComponent({
-			el: boardWrapper,
-			type: RENDER_TYPES.TMPL,
-		});
-		board.data = JSON.parse(JSON.stringify(users));
-		board.render();
-	} else {
-		const em = document.createElement('em');
-		em.textContent = 'Loading';
-		leaderboardSection.appendChild(em);
-		
-		AjaxModule.doPromisePost({
-			path: '/leaders',
-			body: {
-				page: pageNumber,
-				items: itemsNumber,
-			},
-		})
-			.then( response => {
-				console.log('Response status: ' + response.status);
-				return response.json();
-			})
-			.then( users => {
-				console.log(users);
-				application.innerHTML = '';
-				createLeaderboard(users, pageNumber);
-				
-			})
-			.catch( () => {
-				console.error;
-				application.innerHTML = '';
-				createMenu();
-			});
-		
-	}
-	const prev = document.createElement('input');
-	prev.value = "Предыдущая страница"
-	prev.type = "button"
-	prev.addEventListener("click", function(){
-		AjaxModule.doPromisePost({
-			path: '/leaders',
-			body: {
-				page: pageNumber - 1,
-				items: itemsNumber,
-			},
-		})
-			.then( response => {
-				console.log('Response status: ' + response.status);
+    AjaxModule.doPromisePost({
+      path: '/leaders',
+      body: {
+        page: pageNumber,
+        items: itemsNumber,
+      },
+    })
+      .then((response) => {
+        console.log(`Response status: ${response.status}`);
+        return response.json();
+      })
+      .then((users) => {
+        console.log(users);
+        application.innerHTML = '';
+        createLeaderboard(users, pageNumber);
+      })
+      .catch(() => {
+        console.error;
+        application.innerHTML = '';
+        createMenu();
+      });
+  }
+  const prev = document.createElement('input');
+  prev.value = 'Предыдущая страница';
+  prev.type = 'button';
+  prev.addEventListener('click', () => {
+    AjaxModule.doPromisePost({
+      path: '/leaders',
+      body: {
+        page: pageNumber - 1,
+        items: itemsNumber,
+      },
+    })
+      .then((response) => {
+        console.log(`Response status: ${response.status}`);
 
-				return response.json();
-			})
-			.then( users => {
-				console.log(users);
-				application.innerHTML = '';
-				createLeaderboard(users, pageNumber - 1);
-			})
-			.catch( () => {
-				console.error;
-				application.innerHTML = '';
-				createMenu();
-			});
-	});
+        return response.json();
+      })
+      .then((users) => {
+        console.log(users);
+        application.innerHTML = '';
+        createLeaderboard(users, pageNumber - 1);
+      })
+      .catch(() => {
+        console.error;
+        application.innerHTML = '';
+        createMenu();
+      });
+  });
 
-	const next = document.createElement('input');
-	next.value = "Следующая страница"
-	next.type = "button"
-	next.addEventListener("click", function(){
-		AjaxModule.doPromisePost({
-			path: '/leaders',
-			body: {
-				page: pageNumber + 1,
-				items: itemsNumber,
-			},
-		})
-			.then( response => {
-				console.log('Response status: ' + response.status);
+  const next = document.createElement('input');
+  next.value = 'Следующая страница';
+  next.type = 'button';
+  next.addEventListener('click', () => {
+    AjaxModule.doPromisePost({
+      path: '/leaders',
+      body: {
+        page: pageNumber + 1,
+        items: itemsNumber,
+      },
+    })
+      .then((response) => {
+        console.log(`Response status: ${response.status}`);
 
-				return response.json();
-			})
-			.then( users => {
-				console.log(users);
-				application.innerHTML = '';
-				createLeaderboard(users, pageNumber + 1);
-			})
-			.catch( () => {
-				console.error;
-				application.innerHTML = '';
-				createMenu();
-			});
-	});
+        return response.json();
+      })
+      .then((users) => {
+        console.log(users);
+        application.innerHTML = '';
+        createLeaderboard(users, pageNumber + 1);
+      })
+      .catch(() => {
+        console.error;
+        application.innerHTML = '';
+        createMenu();
+      });
+  });
 
-	application.appendChild(leaderboardSection);
-	application.appendChild(prev);
-	application.appendChild(next);
+  application.appendChild(leaderboardSection);
+  application.appendChild(prev);
+  application.appendChild(next);
 }
 
 /**
  * Создание страницы профиля пользователя
  */
 
-function createProfile (me) {
-	const profileSection = document.createElement('section');
-	profileSection.dataset.sectionName = 'profile';
+function createProfile(me) {
+  const profileSection = document.createElement('section');
+  profileSection.dataset.sectionName = 'profile';
 
-	if (me) {
-		const profile = new ProfileComponent({
-			el: profileSection,
-			type: RENDER_TYPES.DOM,
-		});
-		profile.data = JSON.parse(JSON.stringify(me));
-		profile.render();
-	} else {
-		AjaxModule.doPromiseGet({
-			path: '/me',	
-		})
-			.then( response => {
-				console.log('Response status: ' + response.status);
-				return response.json();
-			})
-			.then( user => {
-				console.log(user);
-				application.innerHTML = '';
-				createProfile(user);
-			})
-			.catch( () => {
-				application.innerHTML = '';
-				createMenu('Unauthorized');
-				return;
-			});
-	}
+  if (me) {
+    const profile = new ProfileComponent({
+      el: profileSection,
+      type: RENDER_TYPES.DOM,
+    });
+    profile.data = JSON.parse(JSON.stringify(me));
+    profile.render();
+  } else {
+    AjaxModule.doPromiseGet({
+      path: '/me',
+    })
+      .then((response) => {
+        console.log(`Response status: ${response.status}`);
+        return response.json();
+      })
+      .then((user) => {
+        console.log(user);
+        application.innerHTML = '';
+        createProfile(user);
+      })
+      .catch(() => {
+        application.innerHTML = '';
+        createMenu('Вы не авторизованы');
+      });
+  }
 
-	application.appendChild(profileSection);
-	application.appendChild(createMenuLink());
+  application.appendChild(profileSection);
+  application.appendChild(createMenuLink());
 }
 /**
  * Создание страницы с информацией об игре
@@ -353,7 +349,7 @@ function signOut() {
     })
     .catch(() => {
       application.innerHTML = '';
-      createMenu('Unauthorized');
+      createMenu('Вы не авторизованы');
     });
 }
 
@@ -386,4 +382,3 @@ application.addEventListener('click', (event) => {
 
   pages[link.dataset.href]();
 });
-
--- public/modules/ajax.js
+++ public/modules/ajax.js
@@ -2,42 +2,43 @@
   const noop = () => null;
   const home = 'http://localhost:3000';
 
-	/** Класс модуля для работы с Ajax. */
-	class AjaxModule {
-		/**
+  /** Класс модуля для работы с Ajax. */
+  class AjaxModule {
+    /**
          * Послать ajax POST-запрос c помощью Promise.
          * @param [path = '/'] адрес запроса
          * @param [body = {}] тело запроса
          */
-		doPromisePost({
-			path = '/',
-			body = {},
-		} = {}) {
-			return fetch(home + path, {
-				method: 'POST',
-				mode: 'cors',
-				credentials: 'include',
-				headers: {
-					'Content-Type': 'application/json; charset=utf-8',
-				},
-				body: JSON.stringify(body)
-			});
-		}
-		/**
+    doPromisePost({
+      path = '/',
+      body = {},
+    } = {}) {
+      return fetch(home + path, {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify(body),
+      });
+    }
+
+    /**
          * Послать ajax GET-запрос c помощью Promise.
          * @param [path = '/'] адрес запроса
          */
-		doPromiseGet({
-			path = '/',
-		} = {}) {
-			return fetch(home + path, {
-				method: 'GET',
-				mode: 'cors',
-				credentials: 'include',
-				body: null,
-			});
-		}
-	}
+    doPromiseGet({
+      path = '/',
+    } = {}) {
+      return fetch(home + path, {
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include',
+        body: null,
+      });
+    }
+  }
 
   window.AjaxModule = new AjaxModule();
 }());
