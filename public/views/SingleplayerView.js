30704ba25ab3e8ffa7e120e935cf97f4e21aba6a
Vitaly <79vitaly79@gmail.com>
2019-04-15 17:19:16 +0300

    Fixed dsize

--- public/views/SingleplayerView.js
+++ public/views/SingleplayerView.js
@@ -13,23 +13,17 @@ export default class SingleplayerView extends BaseView {
     render() {
         this.el.innerHTML = '';
         // TODO: ПОТОМ НОРМАЛЬНО СДЕЛАЕМ
-        const mainSection = document.createElement('div');
-		mainSection.dataset.sectionName = 'main';
-        const title = document.createElement('h1');
-        title.textContent = 'Здесь будет игра в режиме Singleplayer';
-        title.style = 'color: white; display: flex; align-items: center; justify-content: center; font-size: 50px;';
-        mainSection.appendChild(title);
-        this.el.appendChild(mainSection);
 
         let canvas = document.createElement('canvas');
         canvas.id = "gc";
-        canvas.width = 1200;
-        canvas.height = 1200;
+        canvas.width = 600;
+        canvas.height = 600;
         canvas.style.position = "absolute";
         this.el.appendChild(canvas);
 
         let score = document.createElement('h1');
         score.id = "score";
+        score.style = 'color: white; display: flex; align-items: center; justify-content: center; font-size: 50px;';
         this.el.appendChild(score);
         const name = new Game(arguments);
     }
