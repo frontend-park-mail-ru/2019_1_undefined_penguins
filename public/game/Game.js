5abf33580f9ed89afd0f01900600d69615ffd1d6
Betchika99 <dobrl2011@mail.ru>
2019-04-20 06:02:43 +0300

    FRONT-30c: Updated game animation

--- public/game/Game.js
+++ public/game/Game.js
@@ -59,11 +59,13 @@ export default class Game {
   drawCanvas() {
     const ctx = this.ctx;
     const pisces = this.pisces;
-    const fishWidth = 60;
-    const fishHeigth = 30;
+    const fishWidth = 30;
+    const fishHeigth = 15;
     const penguinPosition = this.penguinPosition
     //СТЕРЕТЬ ВСЁ
-    ctx.clearRect(0, 0, this.canv.width, this.canv.height);
+    if (this.bullet.shoted === false) {
+      ctx.clearRect(0, 0, this.canv.width, this.canv.height);
+    }
 
     //ОТРИСОВКА РЫБОК
     for (let i = 0; i < this.pisces.length; i++) {
@@ -72,7 +74,7 @@ export default class Game {
       const fishImage = new Image();
 
       fishImage.onload = function () {
-        ctx.drawImage(fishImage, pisces[i].x, pisces[i].y, fishWidth, fishHeigth);
+        ctx.drawImage(fishImage, pisces[i].x-fishWidth/2, pisces[i].y-fishHeigth/2, fishWidth, fishHeigth);
       };
       fishImage.src = "../images/fish-3.png";
     }
@@ -103,6 +105,41 @@ export default class Game {
     // }
     // ctx.translate(-this.penguinPosition.x, -this.penguinPosition.y);
 
+    //ОТРИСОВКА ПУЛИ
+    // this.bulletImage.src = "img/snow-1.png";
+    // let bulletWidth = 20;
+    // let bulletHeight = 20;
+    // this.ctx.drawImage(this.bulletImage, this.bullet.x-bulletWidth/2, this.bullet.y-bulletHeight/2, bulletWidth, bulletHeight);
+
+    const bulletImage = new Image();
+    const bullet = this.bullet
+    const bulletWidth = 20;
+    const bulletHeight = 20;
+    const lastBullet = this.lastBullet;
+    bulletImage.onload = function () {
+      if (lastBullet) {
+        ctx.clearRect(lastBullet.x - bulletWidth / 2-10, lastBullet.y - bulletHeight / 2-10, bulletWidth+20, bulletHeight+20);
+      }
+      ctx.drawImage(bulletImage, bullet.x - bulletWidth / 2, bullet.y - bulletHeight / 2, bulletWidth, bulletHeight);
+
+    };
+    bulletImage.src = "../images/snow-1.png";
+    this.lastBullet = this.bullet;
+
+    //ОТРИСОВКА ПУШКИ
+    // const gunWidth = 130;
+    // const gunHeigth = 130;
+    // this.gunImage.src = "cloud.png";
+    // this.ctx.drawImage(this.gunImage, this.canv.width/2-gunWidth/2, this.canv.height/2-gunHeigth/2, gunWidth, gunHeigth);
+    const canv = this.canv
+    const gunImage = new Image();
+    const gunWidth = 130;
+    const gunHeigth = 130;
+    gunImage.onload = function () {
+      ctx.drawImage(gunImage, canv.width / 2 - gunWidth / 2, canv.height / 2 - gunHeigth / 2, gunWidth, gunHeigth);
+    };
+    gunImage.src = "../images/cloud.png";
+
     const penguinImage = new Image();
 
     penguinImage.onload = function () {
@@ -121,38 +158,7 @@ export default class Game {
       }
       ctx.translate(-penguinPosition.x, -penguinPosition.y);
     };
-
     penguinImage.src = "../images/penguin-2.png";
-
-    //ОТРИСОВКА ПУЛИ
-    // this.bulletImage.src = "img/snow-1.png";
-    // let bulletWidth = 20;
-    // let bulletHeight = 20;
-    // this.ctx.drawImage(this.bulletImage, this.bullet.x-bulletWidth/2, this.bullet.y-bulletHeight/2, bulletWidth, bulletHeight);
-
-    const bulletImage = new Image();
-    const bullet = this.bullet
-    const bulletWidth = 20;
-    const bulletHeight = 20;
-    bulletImage.onload = function () {
-      ctx.drawImage(bulletImage, bullet.x - bulletWidth / 2, bullet.y - bulletHeight / 2, bulletWidth, bulletHeight);
-
-    };
-    bulletImage.src = "../images/snow-1.png";
-
-    //ОТРИСОВКА ПУШКИ
-    // const gunWidth = 130;
-    // const gunHeigth = 130;
-    // this.gunImage.src = "cloud.png";
-    // this.ctx.drawImage(this.gunImage, this.canv.width/2-gunWidth/2, this.canv.height/2-gunHeigth/2, gunWidth, gunHeigth);
-    const canv = this.canv
-    const gunImage = new Image();
-    const gunWidth = 130;
-    const gunHeigth = 130;
-    gunImage.onload = function () {
-      ctx.drawImage(gunImage, canv.width / 2 - gunWidth / 2, canv.height / 2 - gunHeigth / 2, gunWidth, gunHeigth);
-    };
-    gunImage.src = "../images/cloud.png";
   }
 
   game() {
