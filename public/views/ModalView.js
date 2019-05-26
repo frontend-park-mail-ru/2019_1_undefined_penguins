a6f340e726594569dca6750213d9ecd1b49f77b1
Betchika99 <dobrl2011@mail.ru>
2019-05-27 01:53:26 +0300

    FRONT-41: Added precommit with linter

--- public/views/ModalView.js
+++ public/views/ModalView.js
@@ -4,19 +4,29 @@ import Bus from '../scripts/EventBus.js';
 export default class ModalView {
     constructor (el) {
         this.el = el;
-        this.message = null;
+        this.data = {};
     }
 
     show (flag) {
         switch (flag) {
         case 'NOT_NET': {
-            this.message = 'Чтобы продолжить, нужен Интернет';
+            this.data = {
+                message: 'Чтобы продолжить, нужен Интернет',
+                flag: 'net',
+            };
             break;
         }
         case 'NOT_AUTH': {
-            this.message = 'Чтобы продолжить, необходимо Войти';
+            this.data = {
+                message: 'Чтобы продолжить, необходимо Войти',
+                flag: 'auth',
+            };
             break;
         }
+        }
+        const section = document.getElementsByClassName('modal-section')[0];
+        if (section) {
+            this.el.removeChild(section);
         }
         this.render();
         const modal = document.getElementsByClassName('modal')[0];
@@ -26,13 +36,14 @@ export default class ModalView {
     render () {
         const section = document.createElement('section');
         section.classList = 'modal-section';
-        section.innerHTML = ModalTmpl(this.message);
+        section.innerHTML = ModalTmpl(this.data);
         this.el.appendChild(section);
         
         const modal = document.getElementsByClassName('modal')[0];
         const span = document.getElementsByClassName('modal__close')[0];
         const signInBitton = document.getElementsByClassName('sign-in-button')[0];
         const signUpBitton = document.getElementsByClassName('sign-up-button')[0];
+        const okBitton = document.getElementsByClassName('ok-button')[0];
         
         span.addEventListener('click', (event) => {
             event.preventDefault();
@@ -46,16 +57,23 @@ export default class ModalView {
             }
         });
 
-        signInBitton.addEventListener('click', (event) => {
-            event.preventDefault();
-            modal.style.display = 'none';
-            Bus.emit('open-sign-in');
-        });
-
-        signUpBitton.addEventListener('click', (event) => {
-            event.preventDefault();
-            modal.style.display = 'none';
-            Bus.emit('open-sign-up');
-        });
+        if (signInBitton) {
+            signInBitton.addEventListener('click', (event) => {
+                event.preventDefault();
+                modal.style.display = 'none';
+                Bus.emit('open-sign-in');
+            });
+    
+            signUpBitton.addEventListener('click', (event) => {
+                event.preventDefault();
+                modal.style.display = 'none';
+                Bus.emit('open-sign-up');
+            });
+        } else {
+            okBitton.addEventListener('click', (event) => {
+                event.preventDefault();
+                modal.style.display = 'none';
+            });
+        }
     }
 }
