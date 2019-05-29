0aa810a5ed2d92e6af5fc0ff03d029c0626ec7cb
Kate <kirilek@bk.ru>
2019-05-29 17:24:30 +0300

    added restart services

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/bash
 
 ssh-keyscan -H $PRODUCTION_MACHINE_ADDRESS >> ~/.ssh/known_hosts
-chmod 600 ./deploymen_travis_key
-ssh -i ./deploymen_travis_key travis@$PRODUCTION_MACHINE_ADDRESS << EOF
+chmod 600 ./deployment_travis_key
+ssh -i ./deployment_travis_key travis@$PRODUCTION_MACHINE_ADDRESS << EOF
 cd front
 echo Aquiring fresh version of repo... && sudo git checkout deployment && \
 echo Pulling changes... && sudo git pull && \
