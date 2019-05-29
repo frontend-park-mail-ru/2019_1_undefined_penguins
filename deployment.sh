0a1710030f3ed34db409f5ed62bb780f3294091e
Kate <kirilek@bk.ru>
2019-05-29 17:23:30 +0300

    added restart services

---
+++ deployment.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+
+ssh-keyscan -H $PRODUCTION_MACHINE_ADDRESS >> ~/.ssh/known_hosts
+chmod 600 ./deploymen_travis_key
+ssh -i ./deploymen_travis_key travis@$PRODUCTION_MACHINE_ADDRESS << EOF
+cd front
+echo Aquiring fresh version of repo... && sudo git checkout deployment && \
+echo Pulling changes... && sudo git pull && \
+echo Restarting service... && sudo systemctl restart penguin-front.service && \
+echo Enabling service ... && sudo systemctl enable penguin-front.service && \
+echo Successfully deployed!!!
+exit
+EOF 
