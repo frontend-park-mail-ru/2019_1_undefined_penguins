a1fcd7e558ed75d67634bd99b77a9fe45a54218a
Betchika99 <dobrl2011@mail.ru>
2019-05-29 19:36:18 +0300

    deploy: fix travis again

--- deployment.sh
+++ deployment.sh
@@ -2,7 +2,8 @@
 
 ssh-keyscan -H $PRODUCTION_MACHINE_ADDRESS >> ~/.ssh/known_hosts
 chmod 600 ./deployment_travis_key
-ssh -i ./deployment_travis_key travis@$PRODUCTION_MACHINE_ADDRESS << EOF
+cat ./deployment_travis_key
+ssh -i ./deployment_travis_key -v travis@$PRODUCTION_MACHINE_ADDRESS << EOF
 cd front
 echo Aquiring fresh version of repo... && git checkout deployment && \
 echo Pulling changes... && git pull && \
